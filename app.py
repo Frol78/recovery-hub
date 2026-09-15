9a70399f195c207653fe7b1a9d24e9c8adeb2489
Frol78 <ei4336344@gmail.com>
2026-09-15 21:21:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -150,4 +150,5 @@ def reset():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
